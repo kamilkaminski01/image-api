cf72b2cd10d3797918d92c564522608f06626c63
Kamil Kaminski <kamilkaminski39@gmail.com>
2023-09-19 22:55:34 +0200

    Added listing and return data logic according to account tier

--- api/images/serializers.py
+++ api/images/serializers.py
@@ -1,11 +1,10 @@
 from typing import Dict
 
-from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 from thumbnails.models import Thumbnail
 from thumbnails.serializers import ThumbnailSerializer
-from thumbnails.utils import generate_thumbnail
+from thumbnails.utils import create_thumbnail_for_user
 from users.models import User
 
 from .models import Image
@@ -19,24 +18,70 @@ class ImageListSerializer(serializers.ModelSerializer):
         model = Image
         fields = ["email", "image", "thumbnails"]
 
-    def get_email(self, obj: Image) -> str:
-        return obj.user.email
+    def to_representation(self, instance):
+        request = self.context.get("request")
+        user = request.user
+
+        if user.account_tier == User.AccountTierChoices.BASIC:
+            thumbnail = instance.thumbnails.first()
+            thumbnail_url = request.build_absolute_uri(thumbnail.thumbnail.url)
+            return {
+                "email": user.email,
+                "thumbnails": {"thumbnail": thumbnail_url},
+            }
+        elif user.account_tier in [
+            User.AccountTierChoices.PREMIUM,
+            User.AccountTierChoices.ENTERPRISE,
+        ]:
+            thumbnails = Thumbnail.objects.filter(image=instance)
+            thumbnails_data = [
+                {"thumbnail": request.build_absolute_uri(thumbnail.thumbnail.url)}
+                for thumbnail in thumbnails
+            ]
+            return {
+                "email": user.email,
+                "image": request.build_absolute_uri(instance.image.url),
+                "thumbnails": thumbnails_data,
+            }
+        return super().to_representation(instance)
 
 
 class ImageSerializer(serializers.ModelSerializer):
+    thumbnails = ThumbnailSerializer(many=True, read_only=True)
+
     class Meta:
         model = Image
-        fields = ["image"]
+        fields = ["image", "thumbnails"]
 
-    def create(self, validated_data: Dict) -> Image:
+    def create(self, validated_data: Dict):
         user = self.context["request"].user
         validated_data["user"] = user
+        filename = validated_data["image"].name
         image = Image.objects.create(**validated_data)
+        basic_thumbnail = create_thumbnail_for_user(user, image, filename, 200)
+        premium_thumbnail = create_thumbnail_for_user(user, image, filename, 400)
         if user.account_tier == User.AccountTierChoices.BASIC:
-            height = 200
-            thumbnail = generate_thumbnail(image.image, height, height)
-            thumbnail_obj = Thumbnail.objects.create(user=user, image=image)
-            thumbnail_obj.thumbnail.save(
-                f"thumbnail-{height}x{height}.jpg", ContentFile(thumbnail)
-            )
-        return image
+            return basic_thumbnail
+        elif user.account_tier in [
+            User.AccountTierChoices.PREMIUM,
+            User.AccountTierChoices.ENTERPRISE,
+        ]:
+            return [image, basic_thumbnail, premium_thumbnail]
+
+    def to_representation(self, instance):
+        if isinstance(instance, Thumbnail):
+            if request := self.context.get("request"):
+                thumbnail_url = request.build_absolute_uri(instance.thumbnail.url)
+                return {"thumbnail": thumbnail_url}
+        elif isinstance(instance, list):
+            if request := self.context.get("request"):
+                image = instance.pop(0)
+                thumbnails_data = [
+                    {"thumbnail": request.build_absolute_uri(thumbnail.thumbnail.url)}
+                    for thumbnail in instance
+                ]
+                return {
+                    "image": request.build_absolute_uri(image.image.url),
+                    "thumbnails": thumbnails_data,
+                }
+        return super().to_representation(instance)
--- api/thumbnails/utils.py
+++ api/thumbnails/utils.py
@@ -1,6 +1,12 @@
 from io import BytesIO
 
+from django.core.files.base import ContentFile
 from PIL import Image as PILImage
+
+from images.models import Image
+from users.models import User
+
+from .models import Thumbnail
 
 
 def generate_thumbnail(image_file: str, width: int, height: int):
@@ -10,3 +16,12 @@ def generate_thumbnail(image_file: str, width: int, height: int):
     img.save(thumbnail_io, format="JPEG")
     thumbnail_io.seek(0)
     return thumbnail_io.getvalue()
+
+
+def create_thumbnail_for_user(
+    user: User, image: Image, filename: str, height: int
+) -> Thumbnail:
+    generated_thumbnail = generate_thumbnail(image.image.file, height, height)
+    thumbnail = Thumbnail.objects.create(user=user, image=image)
+    thumbnail.thumbnail.save(f"{height}px-{filename}", ContentFile(generated_thumbnail))
+    return thumbnail
